023bfb511443afecb75dfe0eb96c49772b525f98
Carl Brown <carlb@pobox.com>
2011-10-04 23:54:37 -0500

    license update

--- RegexOnNSStringIOSExampleTests/RegexOnNSStringIOSExampleTests.m
+++ RegexOnNSStringIOSExampleTests/RegexOnNSStringIOSExampleTests.m
@@ -3,7 +3,7 @@
 //  RegexOnNSStringIOSExampleTests
 //
 //  Created by Carl Brown on 10/3/11.
-//  Copyright 2011 PDAgent, LLC. All rights reserved.
+//  Copyright 2011 PDAgent, LLC. Released under MIT License.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
--- RegexOnNSStringOSXExample/RegexOnNSStringOSXExampleAppDelegate.m
+++ RegexOnNSStringOSXExample/RegexOnNSStringOSXExampleAppDelegate.m
@@ -3,7 +3,7 @@
 //  RegexOnNSString
 //
 //  Created by Carl Brown on 10/3/11.
-//  Copyright 2011 PDAgent, LLC. All rights reserved.
+//  Copyright 2011 PDAgent, LLC. Released under MIT License.
 //
 
 #import "RegexOnNSStringOSXExampleAppDelegate.h"
